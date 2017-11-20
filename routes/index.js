0ce61d994fc5cfdc84de789042eda1d64093d8f5
Ubuntu <ubuntu@ip-172-31-19-152.us-east-2.compute.internal>
2017-11-20 02:14:11 +0000

    Updated routes/index.js to redirect to home.html

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'SunSmart'});
-	//res.sendFile('/public/home.html'); 
+    //res.render('index', { title: 'SunSmart'});
+	res.sendFile(path.join(__dirname, '/../public/home.html')); 
 });
 
 module.exports = router;
